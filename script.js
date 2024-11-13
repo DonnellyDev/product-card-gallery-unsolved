3513ac4f38a1fc1e4c5d96cc1b010d5de840c02c
Eric Donnelly <eric@donnellydev.ca>
2024-11-13 15:29:57 -0500

    initial template push

---
+++ script.js
@@ -0,0 +1,39 @@
+const products = [
+  {
+    id: 1,
+    name: "Smartphone",
+    price: 499.99,
+    image: "assets/phone.jpg",
+  },
+  {
+    id: 2,
+    name: "Laptop",
+    price: 899.99,
+    image: "assets/laptop.jpg",
+  },
+  {
+    id: 3,
+    name: "Headphones",
+    price: 149.99,
+    image: "assets/headphones.jpg",
+  },
+  {
+    id: 4,
+    name: "Smartwatch",
+    price: 249.99,
+    image: "assets/smartwatch.jpg",
+  },
+  {
+    id: 5,
+    name: "Tablet",
+    price: 349.99,
+    image: "assets/tablet.jpg",
+  },
+];
+
+function createProductCard() {
+  // TODO: Implement Me
+}
+function renderProductGallery() {
+  // TODO: Implement me
+}
